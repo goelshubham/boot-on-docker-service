1b0998d08d76756e0cd2f0333d6c076d49665ef5
Shubham Goel <Shubham_Goel@comcast.com>
2019-05-05 10:00:09 -0400

    first commit

---
+++ src/main/java/com/example/bootondockerservice/BootOnDockerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootondockerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootOnDockerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootOnDockerServiceApplication.class, args);
+	}
+
+}
