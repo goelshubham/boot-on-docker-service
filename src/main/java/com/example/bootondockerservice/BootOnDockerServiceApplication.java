bd1c13d79e297a66f72322fed8a7525d5bd9b5cb
Shubham Goel <Shubham_Goel@comcast.com>
2019-05-05 15:37:30 -0400

    Final code change for simple boot app deployed in docker

--- src/main/java/com/example/bootondockerservice/BootOnDockerServiceApplication.java
+++ src/main/java/com/example/bootondockerservice/BootOnDockerServiceApplication.java
@@ -2,12 +2,22 @@ package com.example.bootondockerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api")
 public class BootOnDockerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootOnDockerServiceApplication.class, args);
+	}
+
+	@RequestMapping(value="/home")
+	public String home() {
+		return "Hello Docker World";
+
 	}
 
 }
